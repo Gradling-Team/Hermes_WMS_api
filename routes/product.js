333a2921afd34d36fc4c4f15166c7050770bd384
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2022-10-20 00:40:21 -0400

    fkeofkeo

--- routes/product.js
+++ routes/product.js
@@ -10,4 +10,26 @@ const errHandler = (err) => {
   console.error("Error: ", err);
 };
 /* start request definitions */
+router.get("/:token", async (req, res, next) => {
+  await models.Utilisateur.findOne({
+    where: {
+      Token: req.params.token,
+    },
+  })
+    .then((user) => {
+      if (user.TokenTTL < Date.now()) {
+        res.status(401).send("Token expired");
+      }
+    })
+    .catch((err) => {
+      res.status(404).send("Token not found");
+    });
+  await models.PRODUCT_FORM.findAll({})
+    .then((productForms) => {
+      res.json(productForms);
+    })
+    .catch((err) => {
+      res.status(404).send("No product forms found");
+    });
+});
 module.exports = router;
